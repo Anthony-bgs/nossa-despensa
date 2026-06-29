49f51b3de92df075139d6b135182acd5ca6e9de4
Romancista <anthony-bgs@outlook.com>
2026-06-29 16:11:46 -0300

    começo da nova implementação Nest JS

---
+++ src/helper/produto.ts
@@ -0,0 +1,18 @@
+
+
+
+export enum Categoria {
+
+}
+
+export enum Grandeza {
+    
+}
+
+export enum LocalArmazenamento {
+
+}                                  
+
+export enum StatusProduto {
+    
+}
---
+++ src/produtos/produto.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { Produto } from './produto.insterface ';
+
+@Injectable()
+export class ProdutoService {
+ async novoProduto(dadosdoproduto): Produto { 
+    const novoProduto: 
+  }
+  
+}
